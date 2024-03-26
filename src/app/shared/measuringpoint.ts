3b9c8d9b6f5156ccaa3e840932af72b69d829844
TheoneandonlyChris15 <christian.gebhard2014@gmail.com>
2024-03-26 17:06:04 +0100

    s

--- src/app/shared/measuringpoint.ts
+++
@@ -1,9 +0,0 @@
-export class Measuringpoint {
-  public lastUpdated:Date=new Date();
-  public lastSnowDate:Date=new Date();
-  constructor(public LastUpdate:string, public SnowHeight:string, public newSnowHeight: string, public LastSnowDate:string,
-    public Shortname: string, public Temperature:string) {
-    this.lastUpdated=new Date(LastUpdate);
-    this.lastSnowDate=new Date(LastSnowDate);
-  }
-}
